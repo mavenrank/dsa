0899f2f2e50b371b3c983019da2ba0803ef4b095
mavenrank <Maverickodes.mac@gmail.com>
2025-10-20 19:05:27 +0530

    complete all types of triangles, now going to merge two triangles

--- problems/cpp/patterns/star-patterns/star_patterns.cpp
+++ problems/cpp/patterns/star-patterns/star_patterns.cpp
@@ -110,7 +110,63 @@ void block_reversed_right_trangle(int n){
 }
 
 void hollow_reversed_right_triangle(int n){
-    
+    for (int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j<n-i-1 && i<n-1){
+                cout << " ";
+            }
+            else if(j==n-1 && i<n-1){
+                cout << "*";
+            }
+            else if(j==n-i-1 && i<n-1){
+                cout << "*";
+            }
+            else if(i==n-1){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void block_inverted_reversed_right_triangle(int n){
+    for (int i=n-1;i>=0;i--){
+        for (int j=n-1;j>=0;j--){
+            if (j<=i){
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void hollow_inverted_reversed_right_triangle(int n){
+    for(int i=n-1;i>=0;i--){
+        for (int j=n-1;j>=0;j--){
+            if (i==n-1){
+                cout << "*";
+            }
+            else if (j==i && i>0){
+                cout << "*";
+            }
+            else if(j==0 && i>0){
+                cout << "*";
+            }
+            else if(i==0 & j==0){
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
 }
 void block_pyramid_non_isometric(int n){
     for (int i=0;i<n;i++){
@@ -122,6 +178,89 @@ void block_pyramid_non_isometric(int n){
         }
         for(int k=0;k<i;k++){
             cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void hollow_pyramid_non_isometric(int n){
+    for(int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            if (j<n-i-1){
+                cout << " ";
+            }
+            else if(j==n-i-1 && i<n-1){
+                cout << "*";
+            }
+            else if(i==n-1){
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        for (int k=0;k<i;k++){
+            if (k==i-1 && i<n-1){
+                cout << "*";
+            }
+            else if (i==n-1){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void block_arrowhead_facing_right(int n){
+    for (int i=0;i<n*2-1;i++){
+        if (i<n){
+            for(int j=0;j<n;j++){
+                if(j<=i){
+                    cout << "*";
+                }
+            }
+        }
+        else {
+            for (int k=0;k<n;k++){
+                if (k<n-(i-n)-1){
+                    cout << "*";
+                }
+            }
+        }
+        cout << endl;
+    }
+}
+
+void hollow_arrowhead_facing_right(int n){
+    for (int i=0;i<n*2-1;i++){
+        if(i<n){
+            for(int j=0;j<n;j++){
+                if (j==0 && i<n){
+                    cout << "*";
+                }
+                else if(j==i && i<n){
+                    cout << "*";
+                }
+                else{
+                    cout << " ";
+                }
+            }
+        }
+        else{
+            for (int k=0;k<n;k++){
+                if(k==0){
+                    cout << "*";
+                }
+                else if(k==n-(i-n)-1-1){
+                    cout << "*";
+                }         
+                else{
+                    cout << " ";
+                }
+            }
         }
         cout << endl;
     }
@@ -168,9 +307,35 @@ int main(){
     hollow_reversed_right_triangle(num);
 
     cout << endl;
+    
+    cout << "9. Block Reversed Inverted Right Triangle : " << endl;
+    block_inverted_reversed_right_triangle(num);
 
-    cout << "9. Block Pyramid non-isometric :" << endl;
+    cout << endl;
+
+    cout << "10. Hollow Reversed Inverted Right Triangle : " << endl;
+    hollow_inverted_reversed_right_triangle(num);
+
+    cout << endl;
+
+    cout << "11. Block Pyramid non-isometric :" << endl;
     block_pyramid_non_isometric(num);
+    
+    cout << endl;
 
+    cout << "12. Hollow Pyramid non-isometric : " << endl;
+    hollow_pyramid_non_isometric(num);
+
+    cout << endl;
+
+    cout << "13. Block Arrowhead facing Right : " << endl;
+    block_arrowhead_facing_right(num);
+
+    cout << endl;
+
+    cout << "14. Hollow Arrowhead facing Right : " << endl;
+    hollow_arrowhead_facing_right(num);
+
+    cout << endl;
 
 }
